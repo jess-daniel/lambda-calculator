8659ed00e01193923feeab51a65cb73e7efde303
Jess Daniel <jessdaniel474@gmail.com>
2019-10-08 19:03:36 -0400

    Added all mvp components to DOM

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-const NumberButton = () => {
+
+const NumberButton = props => {
+  console.log("Numbers", props);
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <button>{props.button}</button>
     </>
   );
 };
+
+export default NumberButton;
